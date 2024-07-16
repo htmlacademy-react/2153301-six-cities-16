a00bd07be375448868c760feb637a9a599bffc9e
Evgene Shlyk <serpupstars@gmail.com>
2024-07-17 07:23:03 +1000

    заканчивает первое дз1

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,9 +21,9 @@ function Header(): JSX.Element {
                 </Link>
               </li>
               <li className="header__nav-item">
-                <a className="header__nav-link" to={AppRoute.Root}>
+                <Link className="header__nav-link" to={AppRoute.Root}>
                   <span className="header__signout">Sign out</span>
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
