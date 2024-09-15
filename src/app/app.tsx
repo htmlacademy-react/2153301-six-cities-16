1ce25798b4accbe8e99d443f6a2f2fa6d3cfee03
Evgene Shlyk <serpupstars@gmail.com>
2024-09-16 07:09:33 +1000

    16.09

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,22 +4,25 @@ import {Route, Routes} from 'react-router-dom';
 import MainPage from '@pages/main-page';
 import FavoritePage from "@pages/favorites-page";
 import ProtectRoute from "@components/protect-route/protect-route";
+import NotFoundPage from "@pages/not-found-page";
 
 function App(): JSX.Element {
   return (
     <Routes>
-      <Route
-        path={AppRoute.Root}
-        element={<MainPage offers={OFFERS} locations={CityMap}/>}
-      />
-      <Route
-        path={AppRoute.Favorites}
-        element={
-          <ProtectRoute>
-            <FavoritePage offers={OFFERS}/>
-          </ProtectRoute>
-        }
-      />
+      {/*<Route*/}
+      {/*  path={AppRoute.Root}*/}
+      {/*  element={<MainPage offers={OFFERS} locations={CityMap}/>}*/}
+      {/*/>*/}
+      {/*<Route*/}
+      {/*  path={AppRoute.Favorites}*/}
+      {/*  element={*/}
+      {/*    <ProtectRoute>*/}
+      {/*      <FavoritePage offers={OFFERS}/>*/}
+      {/*    </ProtectRoute>*/}
+      {/*  }*/}
+      {/*/>*/}
+
+      <Route path={AppRoute.NotFound} element={<NotFoundPage/>}/>
 
     </Routes>
   );
