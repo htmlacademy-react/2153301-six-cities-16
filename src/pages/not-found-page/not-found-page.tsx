6bc073d0660f6eec16b3a84551a5ddcbbe5169c7
Evgene Shlyk <serpupstars@gmail.com>
2024-09-18 07:27:07 +1000

    18.09

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,12 +1,12 @@
 import clsx from 'clsx';
-import './not-found-page.scss';
+import style from './not-found-page.module.scss';
 
 function NotFoundPage(): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <main className="page__main page__main--login">
-        <div className={clsx('notFound', 'container')}>
-          <h1 className={'notFound__title'}>404. Page not found</h1>
+        <div className={clsx(style.notFound, 'container')}>
+          <h1 className={style.notFound__title}>404. Page not found</h1>
           <a href='/'>
             Вернуться на главную
           </a>
