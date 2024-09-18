561918424ee1f6b47203a2fea61dd188aaeda63b
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2024-09-18 17:48:25 +1000

    18.09 работа

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,18 +9,18 @@ import NotFoundPage from "@pages/not-found-page";
 function App(): JSX.Element {
   return (
     <Routes>
-      {/*<Route*/}
-      {/*  path={AppRoute.Root}*/}
-      {/*  element={<MainPage offers={OFFERS} locations={CityMap}/>}*/}
-      {/*/>*/}
-      {/*<Route*/}
-      {/*  path={AppRoute.Favorites}*/}
-      {/*  element={*/}
-      {/*    <ProtectRoute>*/}
-      {/*      <FavoritePage offers={OFFERS}/>*/}
-      {/*    </ProtectRoute>*/}
-      {/*  }*/}
-      {/*/>*/}
+      <Route
+        path={AppRoute.Root}
+        element={<MainPage offers={OFFERS} locations={CityMap}/>}
+      />
+      <Route
+        path={AppRoute.Favorites}
+        element={
+          <ProtectRoute>
+            <FavoritePage offers={OFFERS}/>
+          </ProtectRoute>
+        }
+      />
 
       <Route path={AppRoute.NotFound} element={<NotFoundPage/>}/>
 
